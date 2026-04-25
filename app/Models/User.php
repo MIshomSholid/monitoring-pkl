e40a5c08b3d6c8f46ea426d276dbec72ccaf82a4
Ishom Sholid <ishomsholid22@gmail.com>
2026-04-25 11:39:51 +0700

    fix error controller

--- app/Models/User.php
+++ app/Models/User.php
@@ -55,4 +55,9 @@ class User extends Authenticatable
     {
         return $this->hasOne(PembimbingLapangan::class, 'user_id');
     }
+
+    public function scopeActive($query)
+    {
+        return $query->where('is_active', true);
+    }
 }
