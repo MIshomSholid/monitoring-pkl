f029acac06354b2c13b629d55041cc5f0a9d1be5
Ishom Sholid <ishomsholid22@gmail.com>
2026-04-25 10:09:22 +0700

    fix svg eyes regitser

--- resources/views/dashboard/admin/users/create.blade.php
+++ resources/views/dashboard/admin/users/create.blade.php
@@ -63,15 +63,21 @@
                             viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                             <path stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5
-                                           c4.477 0 8.268 2.943 9.542 7
-                                           -1.274 4.057-5.065 7-9.542 7
-                                           -4.477 0-8.268-2.943-9.542-7z" />
+                                               c4.477 0 8.268 2.943 9.542 7
+                                               -1.274 4.057-5.065 7-9.542 7
+                                               -4.477 0-8.268-2.943-9.542-7z" />
                         </svg>
 
                         <!-- Eye Close -->
                         <svg id="eye1-close" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 hidden" fill="none"
                             viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-width="2" d="M3 3l18 18" />
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.875 18.825A10.05 10.05 0 0112 19
+               c-4.477 0-8.268-2.943-9.542-7
+               a9.956 9.956 0 012.042-3.362M6.343 6.343
+               A9.953 9.953 0 0112 5
+               c4.477 0 8.268 2.943 9.542 7
+               a9.96 9.96 0 01-4.132 5.411M15 12
+               a3 3 0 11-6 0 3 3 0 016 0zM3 3l18 18" />
                         </svg>
                     </button>
                 </div>
@@ -96,14 +102,20 @@
                             viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                             <path stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5
-                                           c4.477 0 8.268 2.943 9.542 7
-                                           -1.274 4.057-5.065 7-9.542 7
-                                           -4.477 0-8.268-2.943-9.542-7z" />
+                                               c4.477 0 8.268 2.943 9.542 7
+                                               -1.274 4.057-5.065 7-9.542 7
+                                               -4.477 0-8.268-2.943-9.542-7z" />
                         </svg>
 
                         <svg id="eye2-close" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 hidden" fill="none"
                             viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-width="2" d="M3 3l18 18" />
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.875 18.825A10.05 10.05 0 0112 19
+               c-4.477 0-8.268-2.943-9.542-7
+               a9.956 9.956 0 012.042-3.362M6.343 6.343
+               A9.953 9.953 0 0112 5
+               c4.477 0 8.268 2.943 9.542 7
+               a9.96 9.96 0 01-4.132 5.411M15 12
+               a3 3 0 11-6 0 3 3 0 016 0zM3 3l18 18" />
                         </svg>
                     </button>
                 </div>
