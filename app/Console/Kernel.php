464e5fab880ddc7ae63df881bc857dae2c7abaa5
Ishom Sholid <ishomsholid22@gmail.com>
2026-04-28 17:38:17 +0700

    fix presensi otomatis

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('presensi:generate-alpha')->everyMinute();
+        $schedule->command('presensi:generate-alpha')->dailyAt('00:05');
     }
 
     /**
