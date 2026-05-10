2b04dabf4b03b127223569dbb49427bde0ee5b35
Ishom Sholid <ishomsholid22@gmail.com>
2026-05-10 20:50:21 +0700

    fix route

--- routes/web.php
+++ routes/web.php
@@ -112,11 +112,11 @@ Route::middleware(['auth', 'check.active'])
         Route::resource('/users', UserController::class);
 
         // ================= DATA ADMIN =================
-    
+
         //Data Admin
         Route::get('/admin-data', [AdminDataController::class, 'index'])
             ->name('admin-data.index');
-
+    
         // CREATE
         Route::get('/admin-data/create', [AdminDataController::class, 'create'])
             ->name('admin-data.create');
@@ -268,9 +268,6 @@ Route::middleware(['auth', 'check.active'])
 
         Route::get('/laporan-akhir', [LaporanAkhirController::class, 'index'])
             ->name('laporan-akhir.index');
-
-        Route::get('/laporan-akhir/preview', [LaporanAkhirController::class, 'preview'])
-            ->name('laporan-akhir.preview');
 
         Route::post('/laporan-akhir/preview', [LaporanAkhirController::class, 'preview'])
             ->name('laporan-akhir.preview');
