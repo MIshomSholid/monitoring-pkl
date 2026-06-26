3d18b717ede5c9eb0edc031a751ae3db618d44fb
Ishom Sholid <ishomsholid22@gmail.com>
2026-06-27 01:44:30 +0700

    fix id language

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use Carbon\Carbon;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Carbon::setLocale('id');
         // Paksa HTTPS saat pakai ngrok
         if (request()->header('x-forwarded-proto') === 'https') {
             URL::forceScheme('https');
