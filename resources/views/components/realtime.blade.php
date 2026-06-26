6c4e50061851b2c56cbf96d4d94338aae08524d0
Ishom Sholid <ishomsholid22@gmail.com>
2026-06-26 19:42:03 +0700

    fix realtime configuration

--- resources/views/components/realtime.blade.php
+++ resources/views/components/realtime.blade.php
@@ -1,5 +1,5 @@
 <script>
-    window.REALTIME_URL = "https://accomplished-truth-production-ff71.up.railway.app";
+    window.REALTIME_URL = "https://valiant-laughter-production.up.railway.app";
 
     window.REALTIME_CONFIG = {
         userId: "{{ auth()->id() }}",
@@ -8,7 +8,7 @@
 </script>
 
 <!-- LOAD SOCKET.IO -->
-<script src="https://accomplished-truth-production-ff71.up.railway.app/socket.io/socket.io.js" defer></script>
+<script src="https://valiant-laughter-production.up.railway.app/socket.io/socket.io.js" defer></script>
 
 <!-- LOAD SCRIPT KAMU -->
 <script src="{{ asset('js/realtime.js') }}" defer></script>
