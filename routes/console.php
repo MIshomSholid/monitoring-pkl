f50a04a5bb29d759c7aba9d71276f5ea65cdd4c0
Ishom Sholid <ishomsholid22@gmail.com>
2026-07-07 01:16:06 +0700

    remove duplicate scheduler

--- routes/console.php
+++ routes/console.php
@@ -4,7 +4,9 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('presensi:generate-alpha')->everyMinute();
+Schedule::command('presensi:generate-alpha')->dailyAt('00:05');
+
+//everyMinute()
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
