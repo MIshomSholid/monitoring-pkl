3ea81fbf1cdfda72d633361b95c016a4d8f61b8c
Ishom Sholid <ishomsholid22@gmail.com>
2026-07-07 00:39:40 +0700

    fix generate presensi alpha

--- app/Console/Commands/GenerateAlphaPresensi.php
+++ app/Console/Commands/GenerateAlphaPresensi.php
@@ -19,7 +19,7 @@ class GenerateAlphaPresensi extends Command
         \Log::info('GENERATE ALPHA JALAN: ' . now());
         Carbon::setLocale('id');
 
-        $tanggal = Carbon::today();
+        $tanggal = Carbon::yesterday();
         //today
         $namaHari = strtolower($tanggal->translatedFormat('l'));
 
